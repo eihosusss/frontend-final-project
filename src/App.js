901b220d9aab2f8f9b0bd32ced41064e09301824
eihosusss <a.sundakov1@gmail.com>
2024-08-26 16:51:45 +0200

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <Main />
       <Footer />
-    </div>
+    </Router>
   );
 }
 
